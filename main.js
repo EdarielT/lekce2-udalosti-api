1d89bfdd3778c53eca6d0f3ca1d403aeeedc1e6b
Anastasia Buben <bubenastya@seznam.cz>
2022-02-19 22:34:14 +0100

    Ukol na lekci

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 // tady je místo pro náš program
-console.log('Ahoj');
+//let lev = document.querySelector('p.lev');
+
+let beBold = () => {
+    document.querySelector('.lev').style.fontWeight = 'bold';
+}
