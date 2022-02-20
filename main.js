5bc07e6f2e9be237e6be9ab306a599d9ee185e8d
Anastasia Buben <bubenastya@seznam.cz>
2022-02-20 10:42:50 +0100

    Audio buttons, 3rd exercise

--- main.js
+++ main.js
@@ -12,3 +12,29 @@ let beRed = () => {
 let beBigger = () => {
     lev.style.fontSize = 'larger';
 }
+
+let zvuk = document.getElementById('song');
+
+let startAudio = () => {
+    zvuk.play();
+}
+
+let stopAudio = () => {
+    zvuk.pause();
+}
+
+let muteAudio = () => {
+    zvuk.volume = 0;
+}
+
+let normalVolume = () => {
+    zvuk.volume = 0.5;
+}
+
+let loudAudio = () => {
+    zvuk.volume = 1;
+}
+
+let startAgain = () => {
+    zvuk.currentTime = 0;
+}
